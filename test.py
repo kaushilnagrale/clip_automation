e3536d07c47c19dfcdc6801dd03e063ca0cd54a8
Kaushil <kaushil.nagrale@gmail.com>
2024-10-29 18:53:01 -0700

    editing

--- test.py
+++ test.py
@@ -1,8 +1,10 @@
 import pyperclip
 
-name="Kaushil"
+name=""
 
-pyperclip.copy("Hi i am " + name)
+pyperclip.copy('''Hi '''+ name +'''
+
+I came across your profile and was impressed by your work in [industry/field]. I'm currently [mention your role/area of interest, e.g., "a Data Science student at Arizona State University" or "exploring opportunities in data engineering"], and I would love to connect and learn from your expertise. Looking forward to connecting with you here on LinkedIn!''' + name)
 
 text = pyperclip.paste() 
 print(text) 
